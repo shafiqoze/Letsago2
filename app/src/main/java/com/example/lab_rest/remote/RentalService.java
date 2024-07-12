b660ac1195d57bbfa2f65343bd3599dac6027f10
shafiqoze <shafiqoze@gmail.com>
2024-07-12 11:22:11 +0800

    revert

--- app/src/main/java/com/example/lab_rest/remote/RentalService.java
+++ app/src/main/java/com/example/lab_rest/remote/RentalService.java
@@ -7,7 +7,6 @@ import com.example.lab_rest.model.DeleteResponse;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
